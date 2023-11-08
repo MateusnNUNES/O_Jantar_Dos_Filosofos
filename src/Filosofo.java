e364f7deb19d910ccb69ae610eb4b582b50f4e62
Mateus Nunes <mateus09022004@gmail.com>
2023-11-07 22:06:52 -0300

    agora foi

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Filosofo {
+public class Filosofo extends Thread{
+    private int id;
+    private Garfo garfos;
+    public Filosofo(int id){
+        this.id = id;
+    }
+    private void pensar() {
+        System.out.println("Filósofo " + id + " está pensando.");
+        try {
+            Thread.sleep((long) (Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private void comer() {
+        System.out.println("Filósofo " + id + " está comendo.");
+        try {
+            Thread.sleep((long) (Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.pensar();
+            garfos.pegarGarfo(id);
+            this.comer();
+            garfos.liberarGarfo(id);
+    }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
+    private static final int NUM_FILOSOFOS = 5;
+    private Garfo garfos;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
+            garfos[i] = new Semaphore(1); // Inicialmente, todos os garfos estão disponíveis
+        }
+
+        Filosofo[] filosofos = new Filosofo[NUM_FILOSOFOS];
+
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
+            filosofos[i] = new Filosofo(i);
+            filosofos[i].start();
+        }
+
     }
 }
